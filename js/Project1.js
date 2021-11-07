ba1dd24a18084b407304ad947842155f10ddc013
Abhishek Bhatia <abhishekbhatia@lin.local>
2021-11-07 12:03:04 -0500

    start and attack function update

--- js/Project1.js
+++ js/Project1.js
@@ -1,20 +1,23 @@
 //make variables
 var yugiohApp = {};
-let damage = 0;
-let defense = 0;
 
 //make  arrays upto 3
-
-const cards = ["Blueeyes", "Dark Magician", "RedeyesBlackdragon", "Exxod"];
 const attack = [100, 50, 60, 0];
 const defence = [10, 60, 40, 100];
+let cardImage = [
+  "../images/Blueeyes.jpg",
+  "../images/Dark_Magician.jpg",
+  "../images/BusterBlader.jpg",
+  "../images/EXODIA.jpg",
+];
 
 //make an empty array
-
 let monsters = [];
 
 let pHealth = 1000;
 let cHealth = 1000;
+//let pAttack = pa;
+//let cAttack = ca;
 
 //declare buttons
 
@@ -27,6 +30,9 @@ yugiohApp.playerHealth = document.getElementById("playerHealth");
 yugiohApp.compHealth = document.getElementById("compHealth");
 yugiohApp.playerCard = document.getElementById("playerCard");
 yugiohApp.oppCard = document.getElementById("oppCard");
+yugiohApp.openingImage = document.getElementById("openingImage");
+yugiohApp.compAttack = document.getElementById("compAttack");
+yugiohApp.playerAttack = document.getElementById("playerAttack");
 //make 2 empty boxes to start with to reflext the attack and defense
 
 // functions
@@ -34,10 +40,53 @@ yugiohApp.oppCard = document.getElementById("oppCard");
 function begin() {
   yugiohApp.playerHealth.innerHTML = "Player Health:" + pHealth;
   yugiohApp.compHealth.innerHTML = "Kaiba Health:" + cHealth;
-  yugiohApp.playerCard.src = "../images/Blueeyes.jpg";
+  yugiohApp.playerCard.src = "../images/yugioh.jpg";
+  yugiohApp.oppCard.src = "../images/opponent.jpg";
+
+  //image shuffle algo
+
+  for (let n = cardImage.length - 1; n > 0; n--) {
+    let j = Math.floor(Math.random() * n);
+    let temp = cardImage[n];
+    cardImage[n] = cardImage[j];
+    cardImage[j] = temp;
+  }
+  console.log({ cardImage });
+
+  //Players Hand Algo
+
+  const playerHandcard = cardImage.slice(0, 2);
+
+  const compHandcard = cardImage.slice(1, 3);
+
+  yugiohApp.playerCard.src = playerHandcard[0];
+
+  yugiohApp.oppCard.src = compHandcard[0];
+
+  // Players attack algo
+
+  for (let n = attack.length - 1; n > 0; n--) {
+    let j = Math.floor(Math.random() * n);
+    let temp = attack[n];
+    attack[n] = attack[j];
+    attack[j] = temp;
+  }
+
+  const playerAttack = attack.slice(0, 2);
+
+  const compAttack = attack.slice(1, 3);
+
+  let damage = playerAttack[0];
+  let oAttack = compAttack[0];
+
+  yugiohApp.playerAttack.innerHTML = "Player Attack:" + damage;
+  yugiohApp.compAttack.innerHTML = "Opponent Attack:" + oAttack;
 }
 
-function hit() {}
+function hit() {
+  if (monsters.indexOf(2)) {
+  }
+}
 
 function block() {}
 
@@ -47,3 +96,10 @@ function block() {}
 yugiohApp.startButton.addEventListener("click", begin);
 yugiohApp.pAttackButton.addEventListener("click", hit);
 //yugiohApp.pDefendButton.addEventListener("click", block);
+
+// shuffle the cards
+// for (let n = deck.length - 1; n > 0; n--) {
+//   let j = Math.floor(Math.random() * n);
+//   let temp = deck[n];
+//   deck[n] = deck[j];
+//   deck[j] = temp;
